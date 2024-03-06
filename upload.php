1f8eba239130a9d25900753b6bc8509053f7f28c
Alf <alfgoto@gmail.com>
2024-03-06 13:53:40 +0100

    Upload icon

--- upload.php
+++ upload.php
@@ -18,6 +18,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Yassou title maker -- upload</title>
+    <link rel="icon" href="img/icone.ico">
     <link rel="stylesheet" href="style.css">
 </head>
 
